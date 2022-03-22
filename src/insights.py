3e2cb5631003159326231812325160c6a0ea0c89
Bruno Pinho <brunopinho@outlook.com>
2022-03-22 19:29:49 -0300

    chore: config project

--- src/insights.py
+++ src/insights.py
@@ -1,5 +1,5 @@
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
+    """Checks all different job types and returns a list of them.
 
     Must call `read`
 
