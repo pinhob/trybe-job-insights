3772edb84ae7d0e871046be9aeea4707ca477108
Bruno Pinho <brunopinho@outlook.com>
2022-03-23 13:42:08 -0300

    feat: requisito 3 - get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -56,7 +56,11 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    unique_industries = set()
+
+    for industry in read(path):
+        unique_industries.add(industry["industry"])
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
