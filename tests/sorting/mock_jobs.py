a0ddb1dc2cb539f471f702a2f38693941f699188
Bruno Pinho <brunopinho@outlook.com>
2022-03-29 23:23:51 -0300

    feat: requisito 10 - test_sorting

---
+++ tests/sorting/mock_jobs.py
@@ -0,0 +1,23 @@
+jobs = [
+    {"min_salary": 3000, "max_salary": 4000, "date_posted": "2022-3-29"},
+    {"min_salary": 7000, "max_salary": 10000, "date_posted": "2022-3-15"},
+    {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-3-20"},
+]
+
+jobs_filtered_by_max_salary = [
+    {"min_salary": 7000, "max_salary": 10000, "date_posted": "2022-3-15"},
+    {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-3-20"},
+    {"min_salary": 3000, "max_salary": 4000, "date_posted": "2022-3-29"},
+]
+
+jobs_filtered_by_min_salary = [
+    {"min_salary": 3000, "max_salary": 4000, "date_posted": "2022-3-29"},
+    {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-3-20"},
+    {"min_salary": 7000, "max_salary": 10000, "date_posted": "2022-3-15"},
+]
+
+jobs_filtered_by_date_posted = [
+    {"min_salary": 3000, "max_salary": 4000, "date_posted": "2022-3-29"},
+    {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-3-20"},
+    {"min_salary": 7000, "max_salary": 10000, "date_posted": "2022-3-15"},
+]
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,19 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+from mock_jobs import (
+    jobs,
+    jobs_filtered_by_max_salary,
+    jobs_filtered_by_min_salary,
+    jobs_filtered_by_date_posted,
+)
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "max_salary")
+    assert jobs == jobs_filtered_by_max_salary
+
+    sort_by(jobs, "min_salary")
+    assert jobs == jobs_filtered_by_min_salary
+
+    sort_by(jobs, "date_posted")
+    assert jobs == jobs_filtered_by_date_posted
